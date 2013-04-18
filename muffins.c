f87aa6f21643950d2bc457055d3074a2f4d5ded6
Puja0708 <singh.puja92@hotmail.com>
2013-04-19 01:09:36 +0530

    Create muffins.c

---
+++ muffins.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//#include<conio.h>
+void main()
+{
+int test,noc;
+//clrscr();
+scanf("%d",&test);
+while(test>0)
+{
+scanf("%d",&noc);
+printf("%d\n",(noc/2)+1);
+test--;
+}
+// getch();
+}
